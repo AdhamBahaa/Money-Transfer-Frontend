ced1af37d017ae3406ae75428f1cdbdc86a1b3cf
Adham Bahaa <adhambahaa058@gmail.com>
2024-09-06 00:16:09 +0300

    Finished the Help page

--- src/app/interface/interface.component.ts
+++ src/app/interface/interface.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { MobileAppComponent } from "../shared/mobile-app/mobile-app.component";
 import { FooterComponent } from "../shared/footer/footer.component";
+import { HelpComponent } from "./help/help.component";
 
 @Component({
   selector: 'app-interface',
   standalone: true,
-  imports: [NavbarComponent, MobileAppComponent, FooterComponent],
+  imports: [NavbarComponent, MobileAppComponent, FooterComponent, HelpComponent],
   templateUrl: './interface.component.html',
   styleUrl: './interface.component.scss',
 })
