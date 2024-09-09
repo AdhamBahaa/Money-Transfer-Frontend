e44ed0939e0748bd2c114115691e5f7e027b95c8
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-09 19:57:25 +0300

    move if condition so its not inside constructor

--- src/app/interface/home/home.component.ts
+++ src/app/interface/home/home.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { MobileAppComponent } from '../../shared/mobile-app/mobile-app.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MobileAppComponent],
+  imports: [MobileAppComponent, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   buttonContent!: string;
   loggedIn: boolean = true;
-  constructor() {
+  ngAfterContentInit() {
     if (this.loggedIn) {
       this.buttonContent = 'Create An Account';
     } else {
