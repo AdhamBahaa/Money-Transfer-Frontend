121dc83f1bbe5794059218ba1c63ba2dfc10b425
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 14:25:26 +0300

    create confirmation component and import it to money-transfer

--- src/app/interface/money-transfer/money-transfer.component.ts
+++ src/app/interface/money-transfer/money-transfer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MobileAppComponent } from '../../shared/mobile-app/mobile-app.component';
 import { AmountComponent } from './amount/amount.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @Component({
   selector: 'app-money-transfer',
   standalone: true,
-  imports: [MobileAppComponent, AmountComponent],
+  imports: [MobileAppComponent, AmountComponent, ConfirmationComponent],
   templateUrl: './money-transfer.component.html',
   styleUrl: './money-transfer.component.scss',
 })
