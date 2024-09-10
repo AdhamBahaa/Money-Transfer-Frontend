d29ba6a628dc3b25cca69763a2590524530cf550
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 18:40:42 +0300

    finish ui for payment component

--- src/app/interface/money-transfer/payment/payment.component.ts
+++ src/app/interface/money-transfer/payment/payment.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.scss'
+  styleUrl: './payment.component.scss',
 })
 export class PaymentComponent {
-
+  senderName: string = 'Jonathon Smith';
+  senderAccount: string = 'Account xxxx7890';
+  recipientName: string = 'Asmaa Dosuky ';
+  recipientAccount: string = 'Account xxxx7890';
+  amount: number = 1000;
+  backToHome() {}
+  addToFavourites() {}
 }
