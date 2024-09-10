c6a2e1d9d66fb13d46961ea7b44d70b40e85870f
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 14:22:28 +0300

    create amount component and import it to money-transfer

--- src/app/interface/money-transfer/money-transfer.component.ts
+++ src/app/interface/money-transfer/money-transfer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MobileAppComponent } from '../../shared/mobile-app/mobile-app.component';
+import { AmountComponent } from './amount/amount.component';
 
 @Component({
   selector: 'app-money-transfer',
   standalone: true,
-  imports: [],
+  imports: [MobileAppComponent, AmountComponent],
   templateUrl: './money-transfer.component.html',
-  styleUrl: './money-transfer.component.scss'
+  styleUrl: './money-transfer.component.scss',
 })
-export class MoneyTransferComponent {
-
-}
+export class MoneyTransferComponent {}
