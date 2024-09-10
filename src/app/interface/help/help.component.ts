a28f2be78210f4681fe5fcde592cc96c76970a12
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 21:11:09 +0300

    add navbar and footer to help component

--- src/app/interface/help/help.component.ts
+++ src/app/interface/help/help.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './help.component.html',
-  styleUrl: './help.component.scss'
+  styleUrl: './help.component.scss',
 })
-export class HelpComponent {
-
-}
+export class HelpComponent {}
