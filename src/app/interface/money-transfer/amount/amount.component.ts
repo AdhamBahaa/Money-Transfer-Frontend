67ad9a5a1737ddefeb88dd0d36a96397a9ad7e26
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 16:53:01 +0300

    finish ui for amount component

--- src/app/interface/money-transfer/amount/amount.component.ts
+++ src/app/interface/money-transfer/amount/amount.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-amount',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './amount.component.html',
-  styleUrl: './amount.component.scss'
+  styleUrl: './amount.component.scss',
 })
 export class AmountComponent {
-
+  form = {
+    amount: null,
+    recipientName: '',
+    recipientAccount: null,
+  };
+  onSubmit(form: NgForm) {}
 }
