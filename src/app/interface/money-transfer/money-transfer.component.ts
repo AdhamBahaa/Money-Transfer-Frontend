eadf708f8429b5848e98fc32098103ede8fbfe63
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 14:27:45 +0300

    create payement component and import it to money-transfer

--- src/app/interface/money-transfer/money-transfer.component.ts
+++ src/app/interface/money-transfer/money-transfer.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MobileAppComponent } from '../../shared/mobile-app/mobile-app.component';
 import { AmountComponent } from './amount/amount.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @Component({
   selector: 'app-money-transfer',
   standalone: true,
-  imports: [MobileAppComponent, AmountComponent, ConfirmationComponent],
+  imports: [
+    MobileAppComponent,
+    AmountComponent,
+    ConfirmationComponent,
+    PaymentComponent,
+  ],
   templateUrl: './money-transfer.component.html',
   styleUrl: './money-transfer.component.scss',
 })
