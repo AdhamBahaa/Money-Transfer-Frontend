d76789879971f96ef9f1a58ff8dd5e1c8a14666e
Abdelrhman Ahmed <abdelrhman.main@gmail.com>
2024-09-10 04:20:00 +0300

    My account Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InterfaceComponent } from './interface/interface.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InterfaceComponent],
+  imports: [RouterOutlet, InterfaceComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
