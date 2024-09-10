e94ea7d9738469537246ff87ae1779a055eed184
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-10 21:15:31 +0300

    add navbar,mobile-app and footer to home

--- src/app/interface/home/home.component.ts
+++ src/app/interface/home/home.component.ts
@@ -3,11 +3,19 @@ import { Router } from '@angular/router';
 import { MobileAppComponent } from '../../shared/mobile-app/mobile-app.component';
 import { NgIf } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MobileAppComponent, NgIf, FormsModule],
+  imports: [
+    MobileAppComponent,
+    NgIf,
+    FormsModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -27,6 +35,6 @@ export class HomeComponent {
   };
   onSubmit(form: NgForm) {}
   routeToMoneyTransfer() {
-    this._Router.navigate(['money-transfer']);
+    this._Router.navigate(['money-transfer/confirmation']);
   }
 }
