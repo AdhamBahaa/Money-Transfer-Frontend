94b5039012bcb2e551d699878185ef560e2a36b3
Mariam Ragaie <mariamragaie05@gmail.com>
2024-09-12 10:46:11 +0300

    add correct user info to money transfer pages

--- src/app/interface/money-transfer/confirmation/confirmation.component.ts
+++ src/app/interface/money-transfer/confirmation/confirmation.component.ts
@@ -4,6 +4,11 @@ import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { MobileAppComponent } from '../../../shared/mobile-app/mobile-app.component';
 import { FooterComponent } from '../../../shared/footer/footer.component';
 import { MoneyTransferService } from '../../../services/money-transfer/money-transfer.service';
+import { UserService } from '../../../services/user/user.service';
+import { IUserInfo } from '../../../models/user.model';
+import { Subscription } from 'rxjs';
+import { IAccountUser } from '../../../models/account.model';
+
 @Component({
   selector: 'app-confirmation',
   standalone: true,
@@ -12,18 +17,35 @@ import { MoneyTransferService } from '../../../services/money-transfer/money-tra
   styleUrl: './confirmation.component.scss',
 })
 export class ConfirmationComponent {
-  senderName: string = 'Username';
-  senderAccount: number = 245987890;
+  senderName: string | undefined;
+  senderAccount: IAccountUser[] | undefined;
   recipientName: string = ' ';
   recipientAccount: number = 0;
   amount: number = 0;
+  userInfo: IUserInfo | undefined;
+  private subscription: Subscription | undefined;
   constructor(
     private readonly _Router: Router,
-    private moneyTransferService: MoneyTransferService
+    private moneyTransferService: MoneyTransferService,
+    private readonly _userService: UserService
   ) {}
+  ngOnInit() {
+    this.subscription = this._userService.userInfo().subscribe({
+      next: (res: IUserInfo) => {
+        console.log('INFO: ', res);
+        this.userInfo = res;
+      },
+      error: (error) => {
+        console.error('Complete error:', error);
+      },
+    });
+    this.senderName = this.userInfo?.name || 'Username';
+    this.senderAccount = this.userInfo?.accounts;
+  }
+
   ngDoCheck() {
     const formData = this.moneyTransferService.getFormData();
-    this.recipientName = formData.recipientName || 'Asmaa Dosuky';
+    this.recipientName = formData.recipientName || 'Recipient Name';
     this.recipientAccount = formData.recipientAccount || ' xxxx7890';
     this.amount = formData.amount || 1000;
   }
